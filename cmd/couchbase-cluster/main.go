bde04938fa9699d3d2c7b2686e2879393fb2293c
Traun Leyden <tleyden@couchbase.com>
2015-01-27 08:37:49 -0800

    add kingpin command line args

--- cmd/couchbase-cluster/main.go
+++ cmd/couchbase-cluster/main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"log"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/tleyden/couchbase-cluster-go"
 )
 
+var (
+	app = kingpin.New(
+		"couchbase-cluster",
+		"CLI util for managing a Couchbase Server cluster under CoreOS.",
+	)
+
+	blockUntilRunning = app.Flag(
+		"block-until-running",
+		"Block until all couchbase nodes are detected to be healthy",
+	).Bool()
+
+	etcdServers = app.Flag(
+		"etcd-servers",
+		"Comma separated list of etcd servers (ip/port)",
+	).Default("127.0.0.1:4001").String()
+)
+
 func main() {
+	kingpin.Parse()
+	log.Printf("blockUntilRunning: %v, etcdServers: %v", *blockUntilRunning, *etcdServers)
 
 	couchbaseCluster := &cbcluster.CouchbaseCluster{}
 	log.Printf("couchbase cluster: %v", couchbaseCluster)
