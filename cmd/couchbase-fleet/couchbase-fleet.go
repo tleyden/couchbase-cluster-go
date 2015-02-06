456d8a63f4136db26838b9c7e5f91ba4062fabfc
Traun Leyden <tleyden@couchbase.com>
2015-02-06 04:15:22 +0000

    fix docopt args

--- cmd/couchbase-fleet/couchbase-fleet.go
+++ cmd/couchbase-fleet/couchbase-fleet.go
@@ -12,7 +12,7 @@ func main() {
 	usage := `Couchbase-Fleet.
 
 Usage:
-  couchbase-fleet launch-cbs --version=<cb-version> --num-nodes=<num_nodes> --userpass=<user:pass> [--etcd-servers=<server-list>] [--docker-tag=<docker-tag>] [--skip-clean-slate-check]
+  couchbase-fleet launch-cbs --version=<cb-version> --num-nodes=<num_nodes> --userpass=<user:pass> [--etcd-servers=<server-list>] [--docker-tag=<dt>] [--skip-clean-slate-check]
   couchbase-fleet -h | --help
 
 Options:
@@ -21,12 +21,13 @@ Options:
   --num-nodes=<num_nodes> number of couchbase nodes to start
   --userpass <user:pass> the username and password as a single string, delimited by a colon (:)
   --etcd-servers=<server-list>  Comma separated list of etcd servers, or omit to connect to etcd running on localhos
+  --docker-tag=<dt>  if present, use this docker tag for spawned containers, otherwise, default to "latest"
   --skip-clean-slate-check  if present, will skip the check that we are starting from clean state
-  --docker-tag  if present, use this docker tag for spawned containers, otherwise, default to "latest".
 
 `
-	arguments, _ := docopt.Parse(usage, nil, true, "Couchbase-Fleet", false)
-	log.Printf("args: %v", arguments)
+
+	arguments, err := docopt.Parse(usage, nil, true, "Couchbase-Fleet", false)
+	log.Printf("args: %v.  err: %v", arguments, err)
 
 	if cbcluster.IsCommandEnabled(arguments, "launch-cbs") {
 		if err := launchCouchbaseServer(arguments); err != nil {
